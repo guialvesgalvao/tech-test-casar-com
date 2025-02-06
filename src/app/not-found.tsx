aa2f6374d248f621741457d82b43559f17c2094d
guialvesgalvao <guilhermeg1703@hotmail.com>
2025-02-06 16:01:28 -0300

    ✨Feat: Creating not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,24 @@
+import { PATHS } from "@/consts/paths";
+import Image from "next/image";
+import Link from "next/link";
+import NotFoundImage from '../../public/not-found.png'
+
+
+export default function NotFound(){
+
+    return(
+        <div className="flex h-full flex-col text-center items-center justify-center">
+        <Image 
+        width={200}
+        height={200}
+        alt="Página não encontrada"
+        src={NotFoundImage}
+        />
+        <h1 className="text-4xl font-bold">Página não encontrada</h1>
+        <p className="text-lg text-gray-500">Oops! Parece que essa página não existe.</p>
+        <Link href={PATHS.HOME} className="mt-4 text-primary underline">
+          Voltar para a página inicial
+        </Link>
+      </div>
+    )
+}
