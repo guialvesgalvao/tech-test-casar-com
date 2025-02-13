649ef100d7ead2c50a2fe5eb54f72c01944e35b2
guialvesgalvao <guilhermeg1703@hotmail.com>
2025-02-13 19:25:09 -0300

    ♻️Refactor: Refactor repositories to use Axios interceptors and baseUrl

--- src/repositories/repositoriesRepo.ts
+++ src/repositories/repositoriesRepo.ts
@@ -1,20 +1,32 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 import { IRepositoryResponse } from "../interfaces/IRepository";
 import { LIMIT_REPO_PER_PAGE } from "@/consts/defaultConfigConsts";
 
 export class RepositoriesRepo {
+  private readonly api: AxiosInstance = axios.create({
+    baseURL: "https://api.github.com/",
+  });
   private readonly _quantityPerPage = LIMIT_REPO_PER_PAGE;
+
+  constructor() { 
+    this.api.interceptors.request.use(
+      (config) => {
+        const token = process.env.GITHUB_TOKEN;
+        if (token) {
+          config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+      },
+      (error) => Promise.reject(new Error(error.message))
+    );
+  }
 
   async getUserRepos(
     username: string,
     page: number,
   ): Promise<IRepositoryResponse[] | null> {
-    const endpoint = `https://api.github.com/users/${username}/repos?per_page=${this._quantityPerPage}&page=${page}`;
-    const { data } = await axios.get(endpoint, {
-      headers: {
-        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-      },
-    });
+    const endpoint = `users/${username}/repos?per_page=${this._quantityPerPage}&page=${page}`;
+    const { data } = await this.api.get(endpoint);
 
     return data;
   }
--- src/repositories/usersRepo.ts
+++ src/repositories/usersRepo.ts
@@ -1,25 +1,34 @@
-import axios from "axios";
+import axios, { AxiosInstance }  from "axios";
 
 export class UsersRepo {
-  async getUsersByText(text: string, quantity: number, page: number) {
-    const apiKey = process.env.GITHUB_TOKEN;
-    const endpoint = `https://api.github.com/search/users?q=${text}&per_page=${quantity}&page=${page}`;
-    const { data } = await axios.get(endpoint, {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
+  private readonly api: AxiosInstance = axios.create({
+    baseURL: "https://api.github.com/",
+  });
+
+  constructor() { 
+
+    this.api.interceptors.request.use(
+      (config) => {
+        const token = process.env.GITHUB_TOKEN;
+        if (token) {
+          config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
       },
-    });
+      (error) => Promise.reject(new Error(error.message))
+    );
+  }
+
+  async getUsersByText(text: string, quantity: number, page: number) {
+    const endpoint = `search/users?q=${text}&per_page=${quantity}&page=${page}`;
+    const { data } = await this.api.get(endpoint);
 
     return data.items ?? [];
   }
 
   async getUser(username: string) {
-    const endpoint = `https://api.github.com/users/${username}`;
-    const { data } = await axios.get(endpoint, {
-      headers: {
-        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-      },
-    });
+    const endpoint = `users/${username}`;
+    const { data } = await this.api.get(endpoint);
 
     return data;
   }
